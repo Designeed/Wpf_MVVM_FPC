719f7a9de597b44530c2ec384ab59538446e1e1f
Designeed <v.burczev2013@yandex.ru>
2022-01-26 05:59:21 +0700

    Added User Control for cost

---
+++ FPC/Infrastructure/Styles/UserControl/UCCostTextBox.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+
+namespace FPC.Infrastructure.Styles.UserControl
+{
+    /// <summary>
+    /// Логика взаимодействия для UCCostTextBox.xaml
+    /// </summary>
+    public partial class UCCostTextBox : System.Windows.Controls.UserControl
+    {
+        public static readonly DependencyProperty FieldTextProperty = DependencyProperty.Register("text", typeof(string), typeof(UCCostTextBox), new FrameworkPropertyMetadata("Поле ввода", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public string Text
+        {
+            get => (string)GetValue(FieldTextProperty);
+            set => SetValue(FieldTextProperty, value);
+        }
+
+        public UCCostTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
