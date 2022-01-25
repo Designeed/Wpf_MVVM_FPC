bf71a5b8c67ba16991fadcb1fafd233743f90ae6
Designeed <v.burczev2013@yandex.ru>
2022-01-25 22:52:33 +0700

    Added UserControl for TextBox

---
+++ FPC/Infrastructure/Styles/UserControl/UCTextBox.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace FPC.Infrastructure.Styles.UserControl
+{
+    /// <summary>
+    /// Логика взаимодействия для UCTextBox.xaml
+    /// </summary>
+    public partial class UCTextBox : System.Windows.Controls.UserControl
+    {
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("title", typeof(string), typeof(UCTextBox), new PropertyMetadata("Заголовок"));
+        public static readonly DependencyProperty FieldTextProperty = DependencyProperty.Register("text", typeof(string), typeof(UCTextBox), new FrameworkPropertyMetadata("Поле ввода", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public string Text
+        {
+            get => (string)GetValue(FieldTextProperty);
+            set => SetValue(FieldTextProperty, value);
+        }
+
+        public UCTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
