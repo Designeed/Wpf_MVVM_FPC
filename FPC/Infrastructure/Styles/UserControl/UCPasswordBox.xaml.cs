2ba83743920aa997149fc7ce3b53c7182f4d8308
Designeed <v.burczev2013@yandex.ru>
2022-01-28 15:17:32 +0700

    Fixed bug

--- FPC/Infrastructure/Styles/UserControl/UCPasswordBox.xaml.cs
+++ FPC/Infrastructure/Styles/UserControl/UCPasswordBox.xaml.cs
@@ -7,19 +7,12 @@ namespace FPC.Infrastructure.Styles.UserControl
     /// </summary>
     public partial class UCPasswordBox : System.Windows.Controls.UserControl
     {
-        private static readonly DependencyProperty _titleProperty = DependencyProperty.Register("Title", typeof(string), typeof(UCTextBox), new PropertyMetadata("Заголовок"));
-        private static readonly DependencyProperty _hintProperty = DependencyProperty.Register("Hint", typeof(string), typeof(UCTextBox), new FrameworkPropertyMetadata("Поле ввода", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        private static readonly DependencyProperty _titleProperty = DependencyProperty.Register("Header", typeof(string), typeof(UCTextBox), new PropertyMetadata("Заголовок"));
 
-        public string Title
+        public string Header
         {
             get => (string)GetValue(_titleProperty);
             set => SetValue(_titleProperty, value);
-        }
-
-        public string Hint
-        {
-            get => (string)GetValue(_hintProperty);
-            set => SetValue(_hintProperty, value);
         }
 
         public UCPasswordBox()
