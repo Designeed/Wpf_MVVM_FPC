e58a1d757b2cb9776bcd1e39a0f12e44644a5792
Designeed <v.burczev2013@yandex.ru>
2022-01-30 23:48:11 +0700

    Renamed from MainContentPage to ProductPage

---
+++ FPC/Views/Pages/ProductPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FPC.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductPagePage.xaml
+    /// </summary>
+    public partial class ProductPage : Page
+    {
+        public ProductPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
